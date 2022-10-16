2b8bdfe6181c4b74ea355bf331d87ebcd01ef8c9
shahyad <shahyad.hack@gmail.com>
2022-10-16 10:30:24 +0330

    project started

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom"
+
+// css
+import "./css/custom.css";
+
+// components
+
+
+// contexts
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html","./src/*.js","./src/components/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
